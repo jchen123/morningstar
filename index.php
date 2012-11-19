4ed304ee34e931885cdbe3ff0659fb359a2e8f53
Jeff <jeff@ubuntu.(none)>
2012-11-19 01:49:29 -0800

    init

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Jeff Zhifan Chen</title>
+	<meta name="description" content="The personal portfolio of Jeff Zhifan Chen">
+	<meta name="author" content="Jeff Z. Chen">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+  	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/flexslider.css">
+	<link rel="stylesheet" href="stylesheets/prettyPhoto.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+	
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	================================================== -->
+	
+	
+	<div id="intro">
+	
+		<div class="bg1"></div>
+		
+		<div class="title">
+			<div class="intro-line"></div>
+			<h1 class="small">Jeff Zhifan Chen</h1>
+			<div class="intro-line"></div>
+			<p>Software Engineer living in San Francisco.<br> I love to code, DJ, produce music, write, and explore.<br>My goal is to improve people's lives.</p>
+			<div class="social-intro">
+				<ul>
+					<li><a href="https://twitter.com/jeffzchen"><img src="images/icn-twitter-intro.png" alt="" /></a></li>
+					<li><a href="https://www.facebook.com/jchen123"><img src="images/icn-facebook-intro.png" alt="" /></a></li>
+					<li><a href="http://www.linkedin.com/pub/jeff-chen/14/190/826"><img src="images/icn-linkedin-intro.png" alt="" /></a></li>
+                    <li><a href="http://soundcloud.com/supaflytnt"><img src="images/soundcloud-icon.png" alt="" /></a></li>
+                    <li><a href="http://www.mixcloud.com/supaflytnt/"><img src="images/mixcloud_icon.png" alt="" /></a></li>
+                    <li><a href="http://www.storylane.com/jchen123"><img src="images/storylane_icon.png" alt="" /></a></li>
+                    <li><a href="https://github.com/jchen123"><img src="images/github_icon.png" alt="" /></a></li>
+                    <li><a href="http://www.youtube.com/user/heavendoveabc"><img src="images/youtube.png" alt="" /></a></li>
+				</ul>
+			</div>
+		</div> <!-- end title -->
+	</div> <!-- end intro -->
+	
+	
+	<nav>
+		<ul>
+			<li><a href="#intro">Top</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><a href="#work">Work</a></li>
+			<li><a href="#contact">Contact</a></li>
+            <li><a href="http://arelentlesspursuitof.tumblr.com/">Blog</a></li>
+		</ul>
+		
+		<select name="dropdpown" size="1" onChange="scrollTo(this.value)">
+			<option value="" selected="selected">Menu</option>
+			<option value="#intro">Top</option>
+            <option value="#about">About</option>
+            <option value="#resume">Resume</option>
+            <option value="#work">Work</option>
+			<option value="#contact">Contact</option>
+        </select>
+	</nav>
+
+    <div id="about">
+        <div class="container">
+
+            <div class="sixteen columns">
+                <h2><span class="lines">About</span></h2>
+            </div> <!-- end sixteen columns -->
+
+            <div id="resume" class="eight columns">
+                <p >Born in <a href="http://goo.gl/maps/UOYIZ">Beijing, China</a>, grew up in <a href="http://goo.gl/maps/pvZwR">Northern Virginia</a>.
+                <p >Proud alumni of <a href="http://www.tjhsst.edu/">Thomas Jefferson High School for Science and Technology</a>.<p>
+                <p >Proud alumni of <a href="http://www.cee.org/research-science-institute">Research Science Institute</a>.<p>
+                <p >B.S. at <a href="web.mit.edu">MIT</a> in Computer Science and Mathematics in 2012.<p>
+                <p >English, Castellano Spanish, Mandarin Chinese.<p><br>
+                <p >Download my <a href="http://dl.dropbox.com/u/355677/JeffResume.pdf">Resume</a>.<p>
+            </div>
+
+        </div> <!-- end container -->
+    </div> <!-- end about -->
+
+    <div id="separator3">
+        <div class="bg4"></div>
+    </div>
+
+	<div id="work">
+		<div class="container">
+		
+			<div class="sixteen columns">
+				<h2><span class="lines">Work</span></h2>
+			</div> <!-- end sixteen columns -->
+			
+			<div class="portfolio">
+			
+				<div class="one-third column">
+					<img src="images/fbjob.png" class="scale-with-grid" alt="" />
+					<p class="proj-title">Facebook</p>
+					<p class="proj-desc">Traffic Infrastructure</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+					<img src="images/tuenti.jpg" class="scale-with-grid" alt="" />
+					<p class="proj-title">Tuenti</p>
+					<p class="proj-desc">Ads & Backend</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+					<img src="images/oracle.jpg" class="scale-with-grid" alt="" />
+					<p class="proj-title">Oracle</p>
+					<p class="proj-desc">Fusion Middleware</p>
+				</div> <!-- end one-third column -->
+				
+				<div class="clear"></div>
+				
+				<div class="one-third column">
+                    <img src="images/monster.jpg" class="scale-with-grid" alt="" />
+					<p class="proj-title">Monster</p>
+					<p class="proj-desc">Data & Backend</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+					<a href="images/example-img.png" rel="prettyPhoto" title="Your text here"><img src="images/kakeya.png" class="scale-with-grid" alt="" /></a>
+					<p class="proj-title">Kakeya</p>
+					<p class="proj-desc">Co-Founder</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+					<a href="http://youtu.be/mEcFTpbRiJs" rel="prettyPhoto" title="Your text here"><img src="images/virage.jpg" class="scale-with-grid" alt="" /></a>
+					<p class="proj-title">Autonomy Virage</p>
+					<p class="proj-desc">Researcher</p>
+				</div> <!-- end one-third column -->
+			
+			</div> <!-- end portfolio -->
+		</div> <!-- end container -->
+	</div> <!-- work -->
+	
+	
+	<div id="separator1">
+		<div class="bg2"></div>
+	</div>
+
+	<div id="contact">
+		<div class="container">
+
+            <div class="sixteen columns">
+				<h2 class="white"><span class="lines">Contact</span></h2>
+			</div> <!-- end sixteen columns -->
+
+			<div class="eight columns">
+				<p class="white">Send all inquiries to jchen123 AT gmail.com.</p>
+			</div> 
+
+			<div class="sixteen columns">
+				<div class="copyright">
+					<p>&copy; 2012 All Rights Reserved</p>
+				</div>
+			</div>
+			
+			
+		</div> <!-- end container -->
+		
+	</div> <!-- end contact -->
+	
+
+	
+
+
+	<!-- JS
+	================================================== -->
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script src="javascripts/jquery.sticky.js" type="text/javascript"></script>
+	<script src="javascripts/nbw-parallax.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.inview.js" type="text/javascript"></script>
+	<script src="javascripts/smooth-scroll.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.flexslider.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.prettyPhoto.js" type="text/javascript"></script>
+	<script src="contact-form.js"></script>
+	
+	<!-- Sticky nav -->
+	<script>
+		$(document).ready(function(){
+			$("nav").sticky({topSpacing:0});
+		});
+	</script>
+	
+	
+	<!-- dropdown menu -->
+	<script type="text/javascript">
+    	function scrollTo(target){
+        	var targetPosition = $(target).offset().top;
+        	$('html,body').animate({ scrollTop: targetPosition}, 'slow');
+        }
+    </script>
+	
+	
+	
+	<!-- slider -->
+	<script>
+		$(window).load(function() {
+			$('.flexslider').flexslider({
+				animation: "slide",
+				slideshow: true,
+				slideshowSpeed: 3500,
+				animationSpeed: 1000
+			});
+		});
+	</script>
+	
+	<!-- pretty photo 
+	<script>
+		$(document).ready(function(){
+			$("a[class^='prettyPhoto']").prettyPhoto({
+				social_tools: false,
+				theme: 'light_square'
+			});
+		});
+	</script>
+	-->
+	
+	<script type="text/javascript" charset="utf-8">
+		$(document).ready(function(){
+			$("a[rel^='prettyPhoto']").prettyPhoto({
+				social_tools: false,
+				theme: 'light_square'
+			});
+		});
+	</script>
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
